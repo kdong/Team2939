95980926a7b6899c05b379fafe5279927d73d180
BillyXbot <billyxbot@gmail.com>
2013-10-24 20:32:23 -0700

    New Auto for comp. Ken

---
+++ 2013 FTC Programs/Autonomous.c
@@ -0,0 +1,34 @@
+#pragma config(Hubs,  S1, HTMotor,  HTServo,  none,     none)
+#pragma config(Motor,  mtr_S1_C1_1,     ,              tmotorNone, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     ,              tmotorNone, openLoop)
+
+
+#include "JoystickDriver.c"
+
+int circWheel = 3 * PI; // 9.42
+// 9.42 = 1440 motor encoder ticks
+// 1440 = 9.42 inches
+// 1440 / 9.42 = inches
+
+int inch = 1440 / circWheel;
+int feet = inch * 12;
+
+
+void initializeRobot()
+{
+
+
+  return;
+}
+
+
+
+task main()
+{
+  initializeRobot();
+
+  waitForStart();
+
+  while (true)
+  {}
+}
