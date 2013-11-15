c01a2da4c312bec449512a2ff71716bfb6e4fa2d
Ken <kdong1775@gmail.com>
2013-11-14 22:14:55 -0800

    Updating Teleop.c, AutoFunctions.h, and teleopFunctions.h Need to delete TASK.

--- 2013 FTC Programs/AutoFunctions.h
+++ 2013 FTC Programs/AutoFunctions.h
@@ -1,4 +1,4 @@
-void initializeRobot(){
+				void initializeRobot(){
 	nMotorEncoder[motorRight] = 0;
 	nMotorEncoder[motorLeft] = 0;
 	nMotorEncoder[motorLift] = 0;
--- 2013 FTC Programs/teleopFunctions.h
+++ 2013 FTC Programs/teleopFunctions.h
@@ -1,1 +1,43 @@
-// header file
+// Create better readibility for joystick values
+int motorScale = 0.85;
+
+int joy1X1(){return joystick.joy1_x1;}
+
+int joy1X2(){return joystick.joy1_x2;}
+
+int joy1Y1(){return joystick.joy1_y1;}
+int Ajoy1Y1(){
+	return (motorScale * (pow(joy1Y1()),3) + (1 - motorScale) * (joy1Y1()));
+}
+}
+
+int joy1Y2(){return joystick.joy1_y2;}
+int Ajoy1Y2(){ //adjusted joy1Y2
+	return (motorScale * (pow(joy1Y2()),3) + (1 - motorScale) * (joy1Y2()));
+}
+
+int joy2X1(){return joystick.joy2_x1;}
+
+int joy2X2(){return joystick.joy2_x2;}
+
+int joy2Y1(){return joystick.joy2_y1;}
+
+int joy2Y2(){return joystick.joy2_y2;}
+
+int joyD_Right(){return (joystick.joy2_TopHat == 2;)}
+
+int joyD_Left(){return (joystick.joy2_TopHat == 6;)}
+
+int joyD_Up(){return (joystick.joy2_TopHat == 0;)}
+
+int joyD_Down(){return (joystick.joy2_TopHat == 4;)}
+
+//-------------------------------------------------------//
+int joyD_UpRight(){return joystick.joy2_TopHat == 1;}
+
+int joyD_DownRight(){return joystick.joy2_TopHat == 3;}
+
+int joyD_DownLeft(){return joystick.joy2_TopHat == 5;}
+
+int joyD_UpLeft(){return joystick.joy2_TopHat == 7;}
+//------------------------------------------------------//
